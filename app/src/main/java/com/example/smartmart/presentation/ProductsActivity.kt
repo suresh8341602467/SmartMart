ce2325f040ee4b5e40eeebd9f428bdb8f9737d04
Murthy <murthy.chegondi23@gmail.com>
2026-02-20 18:22:25 +0530

    products

--- app/src/main/java/com/example/smartmart/presentation/ProductsActivity.kt
+++ app/src/main/java/com/example/smartmart/presentation/ProductsActivity.kt
@@ -1,6 +1,10 @@
 package com.example.smartmart.presentation
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class ProductsActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 }
