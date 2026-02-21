7641d1001b4edcae57e825e91ac41fce4853c5a6
sureshcherukuri <chsuresh61@gmail.com>
2026-02-21 09:51:49 +0530

    added packages

--- app/src/main/java/com/example/smartmart/presentation/MainActivity.kt
+++ app/src/main/java/com/example/smartmart/presentation/MainActivity.kt
@@ -2,10 +2,6 @@ package com.example.smartmart.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 
-<<<<<<< sureshbranch
-class MainActivity: AppCompatActivity() {
-
-=======
 class MainActivity : AppCompatActivity(){
->>>>>>> main
+
 }
